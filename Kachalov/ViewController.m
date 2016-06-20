1c5d258f547c12cb0abf86ee0d4d7c0f24ee3d19
ifklid <mulukov.air@gmail.com>
2016-06-21 01:40:37 +0300

    load rought html

--- Kachalov/ViewController.m
+++ Kachalov/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic) NSURLSession *defaultSession;
+@property (nonatomic) NSURLSessionDataTask *dataTask;
 
 @end
 
@@ -17,6 +19,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    if (self.dataTask != nil) {
+//        [self.dataTask cancel];
+//    }
+    
+    
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    self.defaultSession = [NSURLSession sessionWithConfiguration:config];
+    
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.teatrkachalov.ru"];
+    
+    self.dataTask = [self.defaultSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error){
+            NSLog(@"%@", [error localizedDescription]);
+        } else {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                NSLog(@"%@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+            });
+            
+        }
+    }];
+    [self.dataTask resume];
 }
 
 - (void)didReceiveMemoryWarning {
